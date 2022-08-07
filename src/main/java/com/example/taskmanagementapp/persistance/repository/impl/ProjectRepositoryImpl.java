213aa81da39d35b7b57670c4df39541bae649929
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-07 14:46:27 +0300

    add logging

--- src/main/java/com/example/taskmanagementapp/persistance/repository/impl/ProjectRepositoryImpl.java
+++ src/main/java/com/example/taskmanagementapp/persistance/repository/impl/ProjectRepositoryImpl.java
@@ -2,22 +2,28 @@ package com.example.taskmanagementapp.persistance.repository.impl;
 
 import com.example.taskmanagementapp.persistance.model.Project;
 import com.example.taskmanagementapp.persistance.repository.IProjectRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
+@Repository
 public class ProjectRepositoryImpl implements IProjectRepository {
 
     List<Project> projectList = new ArrayList<>();
 
     @Override
     public Optional<Project> findById(Long id) {
+        log.trace("Project Repository >> finding Project by id {}", id);
         return projectList.stream().filter(p -> p.getId().equals(id)).findFirst();
     }
 
     @Override
     public Project save(Project project) {
+        log.trace("Project Repository >> saving Project {}", project);
         Project existingProject = findById(project.getId()).orElse(null);
         if (existingProject == null) {
             projectList.add(project);
--- src/main/java/com/example/taskmanagementapp/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/example/taskmanagementapp/service/impl/ProjectServiceImpl.java
@@ -3,21 +3,27 @@ package com.example.taskmanagementapp.service.impl;
 import com.example.taskmanagementapp.persistance.model.Project;
 import com.example.taskmanagementapp.persistance.repository.IProjectRepository;
 import com.example.taskmanagementapp.service.IProjectService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
+@Service
 public class ProjectServiceImpl implements IProjectService {
 
     @Autowired
     private IProjectRepository projectRepository;
 
     Optional<Project> findById(Long id) {
+        log.debug("project service >> finding project by id {}", id);
         return projectRepository.findById(id);
     }
 
 
     Project save(Project project) {
+        log.debug("project service >> saving project {}", project);
         return projectRepository.save(project);
     }
 }
