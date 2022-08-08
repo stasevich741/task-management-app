a58d13d00849457a338419ee380eba79a8bc1b70
Stanislav Korolkov <stasevich741@gmail.com>
2022-08-08 22:08:02 +0300

    add entity

--- src/main/java/com/example/taskmanagementapp/persistance/model/TaskStatus.java
+++ src/main/java/com/example/taskmanagementapp/persistance/model/TaskStatus.java
@@ -6,13 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TaskStatus {
-    //@formatter:off
     TO_DO("To Do"),
     IN_PROGRESS("In Progress"),
     ON_HOLD("On Hold"),
     DONE("Done");
-    //@formatter:on
 
     private final String label;
-
 }
